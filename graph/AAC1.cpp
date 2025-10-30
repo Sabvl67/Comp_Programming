55a7de39a3756baf7256fc037329304e835d38c1
Sabvl67 <volequynhanh67@gmail.com>
2025-10-30 11:26:03 -0600

    graph

---
+++ graph/AAC1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N, M;
+        cin >> N >> M;
+
+        vector<vector<int>> adj(N + 1);
+        for (int i = 0; i < M; i++) {
+            int X, Y;
+            cin >> X >> Y;
+            adj[X].push_back(Y);
+            adj[Y].push_back(X); // undirected
+        }
+
+        vector<int> dist(N + 1, -1);
+
+        // BFS queue
+        queue<int> q;
+        q.push(1);
+        dist[1] = 0;
+
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            for (int next : adj[node]) {
+                if (dist[next] == -1) { 
+                    dist[next] = dist[node] + 1;
+                    q.push(next);
+                }
+            }
+        }
+
+        cout << dist[N] << "\n";
+    }
+
+    return 0;
+}
